6ca004a907885b5c36015fc2b0d6388bdda71ded
wang yan <wangcom@gmail.com>
2018-01-09 08:37:47 +0800

    add

--- example/pell/dist/pell.js
+++ example/pell/dist/pell.js
@@ -7,6 +7,11 @@
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var actions = {
+  addBackColor: {
+    icon: '<b>G</b>',
+    title: 'AddBackColor',
+    result: () => exec('BackColor', 'green')
+  },
   // 撤销
   undo: {
     icon: '<b>U</b>',
